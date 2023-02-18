e8cfc846987584ec47ba6bfc2fa94452e4ecb137
oleksandr <oleksandr.kovtun2002@gmail.com>
2023-02-18 16:07:16 +0200

    Added hw-3

--- hw-3/script.js
+++ hw-3/script.js
@@ -1,6 +1,11 @@
-let first = prompt('Перше число?:');
+let a, b, c, d, first = prompt('Перше число?:');
 let second = prompt('Друге число?:');
-alert(+first + +second);
-alert(first - second);
-alert(first * second);
-alert(first / second);
+a = String(+first + +second);
+b = String(first - second);
+c = String(first * second);
+d = String(first / second);
+alert('Користувач ввів ' + first  + ' ' + 'і' +  ' ' + second  +  '\n ' +
+    first + ' + ' + second + ' = ' + a +  '\n ' +
+    first + ' - ' + second + ' = ' + b +  '\n ' +
+    first + ' * ' + second + ' = ' + c +  '\n ' +
+    first + ' / ' + second + ' = ' + d)
